9ebe23c3f75298c02855aaa4d54e1c89beac2f0d
Bartimaeus-han <kbtommy9@gmail.com>
2026-07-06 13:20:09 +0900

    Setting mac os environment

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-# 1. Kill any process occupying port 9090
-PID=$(lsof -t -i:9090)
-if [ ! -z ]
+# 1. Build the project using CMake Preset
+echo "Start Server build using macOS Preset..."
+cmake --preset macos-default
+cmake --build build_mac
+
+# 2. Run the server
+if [ $? -eq 0 ]; then
+    echo "Build Success! Run Server..."
+    ./build_mac/SecureWebServer
+else
+    echo "Build Failed, please check error"
+fi
